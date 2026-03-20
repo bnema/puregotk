5bb8c3359c53cc9ea92f457da221af41926468d8
Felicitas Pojtinger <felicitas@pojtinger.com>
2026-03-19 22:04:32 -0700

    Examples: Bump to GNOME 50 and latest library versions
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- examples/mylib-gtk-meson-go/mylibgtkmeson/window.go
+++ examples/mylib-gtk-meson-go/mylibgtkmeson/window.go
@@ -451,6 +451,19 @@ func (x *MainApplicationWindow) Announce(MessageVar string, PriorityVar gtk.Acce
 
 }
 
+// Retrieves the accessible identifier for the accessible object.
+//
+// This functionality can be overridden by `GtkAccessible`
+// implementations.
+//
+// It is left to the accessible implementation to define the scope
+// and uniqueness of the identifier.
+func (x *MainApplicationWindow) GetAccessibleId() string {
+
+	cret := gtk.XGtkAccessibleGetAccessibleId(x.GoPointer())
+	return cret
+}
+
 // Retrieves the accessible parent for an accessible object.
 //
 // This function returns `NULL` for top level widgets.
